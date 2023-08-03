f58ec29233eeec0cc6bac83cc0aa3bbb88d31b7d
Ali Alameedi <alameeda@oregonstate.edu>
2023-08-02 23:13:38 -0700

    Two-fold solution - one prioritizing memory, the other prioritizing time.

---
+++ 217_containsDuplicate.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        # Solution 1:
+        # Time: O(n)
+        # Space: O(n)
+
+        storage = {}
+
+        for val in nums:
+            if val in storage:
+                return True
+            storage[val] = storage.get(val, 0) + 1
+
+        return False
+
+        # Solution 2:
+        # Time: O(n log n) for sorting
+        # Space: O(1)
+
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #
+        #     if nums[i] == nums[i - 1]:
+        #         return True
+        #
+        # return False
